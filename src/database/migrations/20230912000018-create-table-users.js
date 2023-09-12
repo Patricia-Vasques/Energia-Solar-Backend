09222b7e8fcf40a9eb1f46d1cd6d21bd38734165
Patrícia Vasques <patriciahhhh@gmail.com>
2023-09-11 22:00:19 -0300

    fix: Adicionei o created_at, updated_at, deleted_at

--- src/database/migrations/20230912000018-create-table-users.js
+++ src/database/migrations/20230912000018-create-table-users.js
@@ -25,7 +25,19 @@ module.exports = {
     password: {
       type: STRING,
       allowNull: false
-    }
+    }, 
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    deleted_at: {
+      type: Sequelize.DATE,
+      allowNull: true,
+    },
 
     })
   },
