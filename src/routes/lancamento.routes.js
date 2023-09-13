89cf15cc877f5cc359d813e5003e7d90ac711901
Patrícia Vasques <patriciahhhh@gmail.com>
2023-09-13 20:02:56 -0300

    fix: arrumei os conflitos da rota de lançamentos

--- src/routes/lancamento.routes.js
+++ src/routes/lancamento.routes.js
@@ -1,16 +1,7 @@
-
-const { registerRecord, listRecordId, deleteRecord} = require('../controllers/lancamento.controller');
-
-
-
-const {
-  registerRecord,
-  getAllRecords,
-} = require('../controllers/lancamento.controller');
-
->>>>>>> 3271ebbe0ced31a6680ba2d530f08ea83dcbceb6
 const { Router } = require('express');
+const { registerRecord, listRecordId, getAllRecords,  deleteRecord} = require('../controllers/lancamento.controller');
 const { auth } = require ('../middleware/auth')
+
 
 class RecordRouter {
   routesFromRecord() {
@@ -20,12 +11,7 @@ class RecordRouter {
     recordRoutes.post('/v1/geracao', auth, registerRecord);
     recordRoutes.get('/v1/geracao/:unidadeId', auth, listRecordId);
     recordRoutes.delete('/v1/geracao/:id', auth, deleteRecord)
-
-    //   GET - /api/v1/geracao : Esta é a rota para a visualização de registros de geração cadastradas no sistema, e ela deve às retornar no corpo da resposta
-    // Respostas:
-    // 200 - Caso a requisição seja bem sucedida
-    recordRoutes.post('/v1/geracao', registerRecord);
-    recordRoutes.get('/v1/geracao', getAllRecords);
+    recordRoutes.get('/v1/geracao', auth, getAllRecords);
 
 
     return recordRoutes;
