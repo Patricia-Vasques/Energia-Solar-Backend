5fd062fe9a95e20e101312a13b1095b76cf48334
Patrícia Vasques <patriciahhhh@gmail.com>
2023-09-13 19:46:34 -0300

    feat: criei a rota de deletar uma unidade geradora

--- src/routes/lancamento.routes.js
+++ src/routes/lancamento.routes.js
@@ -1,4 +1,14 @@
+
 const { registerRecord, listRecordId, deleteRecord} = require('../controllers/lancamento.controller');
+
+
+
+const {
+  registerRecord,
+  getAllRecords,
+} = require('../controllers/lancamento.controller');
+
+>>>>>>> 3271ebbe0ced31a6680ba2d530f08ea83dcbceb6
 const { Router } = require('express');
 const { auth } = require ('../middleware/auth')
 
@@ -6,9 +16,17 @@ class RecordRouter {
   routesFromRecord() {
     const recordRoutes = Router();
 
+
     recordRoutes.post('/v1/geracao', auth, registerRecord);
     recordRoutes.get('/v1/geracao/:unidadeId', auth, listRecordId);
     recordRoutes.delete('/v1/geracao/:id', auth, deleteRecord)
+
+    //   GET - /api/v1/geracao : Esta é a rota para a visualização de registros de geração cadastradas no sistema, e ela deve às retornar no corpo da resposta
+    // Respostas:
+    // 200 - Caso a requisição seja bem sucedida
+    recordRoutes.post('/v1/geracao', registerRecord);
+    recordRoutes.get('/v1/geracao', getAllRecords);
+
 
     return recordRoutes;
   }
