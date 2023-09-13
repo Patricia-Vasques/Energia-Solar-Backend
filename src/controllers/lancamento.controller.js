bda8b06b188b0a6077edea90a6665c190894cf6f
Patrícia Vasques <patriciahhhh@gmail.com>
2023-09-13 19:28:36 -0300

    feat: Criei a lógica para registros de geração cadastradas no sistema de uma unidade específica

--- src/controllers/lancamento.controller.js
+++ src/controllers/lancamento.controller.js
@@ -35,6 +35,29 @@ class RecordController {
       });
     }
   }
+
+  async listRecordId (req, res) {
+    try {
+      const { id } = req.params
+      const record = await Record.findOne({
+        where: { id }})
+
+        if(!record){
+          return res.status(400).json({
+            error: "Id não encontrado!"
+          })
+        }
+      return res.status(200).json({
+        messagem: "Id encontrado com sucesso!"
+        })
+      } catch (error) {
+        return res.status(500).send({
+          message: 'Erro no servidor.',
+          cause: error.message
+        })
+      }
+  }
 }
+  
 
 module.exports = new RecordController();
