8f9a3a0655fadc99befe90e86e8da71dfb039a45
Patrícia Vasques <patriciahhhh@gmail.com>
2023-09-13 19:29:31 -0300

    feat: Criei a rota da listagem de geração pelo id, e adicionei o auth

--- src/routes/lancamento.routes.js
+++ src/routes/lancamento.routes.js
@@ -1,11 +1,13 @@
-const { registerRecord } = require('../controllers/lancamento.controller');
+const { registerRecord, listRecordId } = require('../controllers/lancamento.controller');
 const { Router } = require('express');
+const { auth } = require ('../middleware/auth')
 
 class RecordRouter {
   routesFromRecord() {
     const recordRoutes = Router();
 
-    recordRoutes.post('/v1/geracao', registerRecord);
+    recordRoutes.post('/v1/geracao', auth, registerRecord);
+    recordRoutes.get('/v1/geracao/:unidadeId', auth, listRecordId )
 
     return recordRoutes;
   }
